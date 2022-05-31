d6029f5e54d114168b0a4651862a1149d46a6ce5
engelsjk <jon.engelsman@gmail.com>
2022-05-31 13:06:36 -0700

    add task building id input

--- makefile
+++ makefile
@@ -1,6 +1,8 @@
+build-web:
+	cd web && npm run build && cd ..	
 tidy:
 	go mod tidy
-run: tidy
-	go run main.go
+run: build-web tidy
+	go run main.go config.yaml
 build-linux-amd64:
 	env GOOS=linux GOARCH=amd64 go build -o bin/valid8-go main.go
--- web/src/client/screens/BuildingsScreen.tsx
+++ web/src/client/screens/BuildingsScreen.tsx
@@ -1,10 +1,25 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { Box, Card, Heading, Link, jsx } from "theme-ui";
+import { useRef, useEffect, useState } from "react";
+import { Box, Card, Heading, Input, jsx } from "theme-ui";
+import { useNavigate } from "react-router-dom";
 
 import CenteredContent from "../components/CenteredContent";
 
+
+
+
 const BuildingsScreen = () => {
+
+    const [taskID, setTaskID] = useState("unknown");
+
+    let navigate = useNavigate();
+    const keyUpHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.code === "Enter" || event.code === "NumpadEnter") {
+            navigate(`/tasks/buildings/${taskID}`);
+        }
+    };
+
     return (
         <CenteredContent>
             <Card sx={{ variant: "cards.floating", mb: 5 }}>
@@ -12,7 +27,11 @@ const BuildingsScreen = () => {
                     Valid8 : Tasks : Buildings
                 </Heading>
                 <Box sx={{ fontSize: 1, mt: 3, textAlign: "left" }}>
-                    <Link href="/">Home</Link>
+                    Task ID <Input
+                        sx={{ width: "500px" }}
+                        onChange={e => setTaskID(e.target.value)}
+                        onKeyUp={keyUpHandler}
+                    />
                 </Box>
             </Card>
         </CenteredContent>
