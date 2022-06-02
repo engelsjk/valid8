3590a825ded7966dda7cade41074619eacd5b8ed
engelsjk <jon.engelsman@gmail.com>
2022-06-02 09:47:06 -0700

    add logger date and file output

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"log"
 	"os"
 
@@ -18,18 +17,29 @@ var embeddedFiles embed.FS
 func main() {
 
 	if len(os.Args) != 2 {
-		panic(fmt.Errorf("must provide config path arg"))
+		log.Fatalf("must provide config path arg")
 	}
 
 	configPath := os.Args[1]
 
 	cfg, err := internal.NewConfig(configPath)
 	if err != nil {
-		panic(err)
+		log.Fatalf("error reading config file: %v", err)
 	}
 
 	app := fiber.New()
-	app.Use(logger.New())
+
+	file, err := os.OpenFile("valid8.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	app.Use(logger.New(logger.Config{
+		Format:     "${time}|${ip}|${cookie:attESHr}|${status}|${method}|${path}\n",
+		TimeFormat: "2006-01-02T15:04:05",
+		Output:     file,
+	}))
 
 	handler.NewAPI(cfg.APIDir).Register(app.Group("/api"))
 	handler.NewStatic(cfg.StaticDir, embeddedFiles).Register(app.Group("/"))
